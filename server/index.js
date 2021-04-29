79908b8b9e7c79d09d68116a21f2bdbc7a42869b
marthansluber <stressfulldev@gmail.com>
2021-04-30 01:49:46 +0700

    install packages and connect to database

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    .then(() => app.listen(port, () => console.log(`Server is running on port: ${port}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
